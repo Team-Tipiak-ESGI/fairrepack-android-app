902ffa2c24d127de80b8d2c68e62cd105cf7575b
Astalios <37106010+Astalios@users.noreply.github.com>
2021-06-04 15:45:44 +0200

    raz
    raz

---
+++ app/src/main/java/com/saglissindustries/fairrepack/MainActivity.java
@@ -0,0 +1,14 @@
+package com.saglissindustries.fairrepack;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

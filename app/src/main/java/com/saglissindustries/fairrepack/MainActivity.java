8103d695fe1702eb8c1b01da563ab5f2724201d1
Astalios <37106010+Astalios@users.noreply.github.com>
2021-06-11 13:04:45 +0200

    HTTPRequest

--- app/src/main/java/com/saglissindustries/fairrepack/MainActivity.java
+++ app/src/main/java/com/saglissindustries/fairrepack/MainActivity.java
@@ -3,9 +3,15 @@ package com.saglissindustries.fairrepack;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.saglissindustries.fairrepack.httprequests.RequestHandler;
+
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,11 +25,38 @@ public class MainActivity extends AppCompatActivity {
 
         this.signInBtn = findViewById(R.id.sign_in);
         this.loginText = findViewById(R.id.login);
-        this.passwordText = findViewById(R.id.login);
+        this.passwordText = findViewById(R.id.password);
 
+        RequestAsync connection = new RequestAsync();
+        connection.doInBackground();
+
+        // Creation Stockage Identifiants
         SharedPreferences sharedPreferences = getSharedPreferences("data", MODE_PRIVATE);
+    }
 
+    public class RequestAsync extends AsyncTask<String, String, String> {
+        @Override
+        protected String doInBackground(String... strings) {
+            try {
+                //GET Request
+                //return RequestHandler.sendGet("https://prodevsblog.com/android_get.php");
 
+                // POST Request
+                JSONObject postDataParams = new JSONObject();
+                postDataParams.put("email", loginText);
+                postDataParams.put("password", passwordText);
 
+                return RequestHandler.sendPost("https://pa.quozul.dev/api/user/login.php", postDataParams);
+            } catch (Exception e) {
+                return new String("Exception: " + e.getMessage());
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            if (s != null) {
+                Toast.makeText(getApplicationContext(), s, Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }

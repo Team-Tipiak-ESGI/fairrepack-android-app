1e42fbe6f9b75815d4c724377a7fce29397b972f
Astalios <37106010+Astalios@users.noreply.github.com>
2021-06-13 22:28:09 +0200

    Update AssocActivity.java

--- app/src/main/java/com/saglissindustries/fairrepack/AssocActivity.java
+++ app/src/main/java/com/saglissindustries/fairrepack/AssocActivity.java
@@ -1,19 +1,30 @@
 package com.saglissindustries.fairrepack;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.saglissindustries.fairrepack.httprequests.RequestCallback;
+import com.saglissindustries.fairrepack.httprequests.RequestHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class AssocActivity extends AppCompatActivity {
 
+    SharedPreferences sharedPreferences = getSharedPreferences("data", MODE_PRIVATE);
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.assoc_activity);
+
 
         ListView listView = findViewById(R.id.assoc_view);
         AssocAdapter assocAdapter = new AssocAdapter(AssocActivity.this, getAssoc());
@@ -21,8 +32,32 @@ public class AssocActivity extends AppCompatActivity {
     }
 
     public List<Assoc> getAssoc(){
+        String url = "https://pa.quozul.dev/api/association/read.php";
         List<Assoc> assocs = new ArrayList<>();
-        //TODO: Faire requete API pour recuperer la liste des associations avec le callback
+        try {
+           new RequestHandler(url, "GET", sharedPreferences.getString("token", null), new RequestCallback() {
+           @Override
+           public void run(){
+               try {
+                   // On va avoir un tableau d'associations il faut donc parse le tableau
+                   JSONArray jsonArray = (JSONArray)response.get("items");
+                   for (int i = 0; i < jsonArray.length(); i++) {
+                       //  Recupration de chaque membres des associations
+                       String name = response.get("name").toString();
+                       String descr = response.get("description").toString();
+                       int coin = Integer.parseInt(response.get("coin").toString());
+                       String address = response.get("address").toString();
+                       // Mise en place dans un tableau
+                       assocs.add(new Assoc(name, descr, address, coin));
+                   }
+               } catch (JSONException e){
+                   e.printStackTrace();
+               }
+           }
+           });
+        } catch (Exception e){
+            e.printStackTrace();
+        }
         return assocs;
     }
 }

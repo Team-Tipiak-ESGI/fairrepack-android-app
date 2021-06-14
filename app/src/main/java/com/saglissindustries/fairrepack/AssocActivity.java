cfa94d3a0efeee48390cd332bd23aa08d36cd04c
Astalios <37106010+Astalios@users.noreply.github.com>
2021-06-15 01:51:20 +0200

    Did buttons, and somewhat did the functions behind button
    may some of them missing

--- app/src/main/java/com/saglissindustries/fairrepack/AssocActivity.java
+++ app/src/main/java/com/saglissindustries/fairrepack/AssocActivity.java
@@ -1,7 +1,11 @@
 package com.saglissindustries.fairrepack;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,20 +19,27 @@ import org.json.JSONException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class AssocActivity extends AppCompatActivity {
+public class AssocActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
     SharedPreferences sharedPreferences = getSharedPreferences("data", MODE_PRIVATE);
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
 
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.assoc_activity);
 
-
+        Button back = (Button)findViewById(R.id.back);
         ListView listView = findViewById(R.id.assoc_view);
+
         AssocAdapter assocAdapter = new AssocAdapter(AssocActivity.this, getAssoc());
         listView.setAdapter(assocAdapter);
+
+        back.setOnClickListener(v ->{
+            Intent backIntent = new Intent(AssocActivity.this, LandpageActivity.class);
+            startActivity(backIntent);
+        });
     }
 
     public List<Assoc> getAssoc(){
@@ -59,5 +70,23 @@ public class AssocActivity extends AppCompatActivity {
             e.printStackTrace();
         }
         return assocs;
+    }
+
+    /**
+     * Callback method to be invoked when an item in this AdapterView has
+     * been clicked.
+     * <p>
+     * Implementers can call getItemAtPosition(position) if they need
+     * to access the data associated with the selected item.
+     *
+     * @param parent   The AdapterView where the click happened.
+     * @param view     The view within the AdapterView that was clicked (this
+     *                 will be a view provided by the adapter)
+     * @param position The position of the view in the adapter.
+     * @param id       The row id of the item that was clicked.
+     */
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
     }
 }
--- app/src/main/java/com/saglissindustries/fairrepack/LandpageActivity.java
+++ app/src/main/java/com/saglissindustries/fairrepack/LandpageActivity.java
@@ -1,7 +1,89 @@
 package com.saglissindustries.fairrepack;
 
+import android.app.AlertDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.widget.Toast;
+
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.saglissindustries.fairrepack.httprequests.RequestCallback;
+import com.saglissindustries.fairrepack.httprequests.RequestHandler;
+
+import org.json.JSONException;
+
 public class LandpageActivity extends AppCompatActivity {
+
+    TextView quant, totalSpent;
+    Button disconnect, convert, assocs;
+    String recupQuant, recupSpent;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landpage);
+
+        //recuperation token
+        SharedPreferences sharedPreferences = getSharedPreferences("data", MODE_PRIVATE);
+
+        // url pour api
+        String url = "https://pa.quozul.dev/api/user/read.php";
+
+        //Creation des Boutons
+        disconnect = (Button)findViewById(R.id.disconnect);
+        convert = (Button)findViewById(R.id.requestConvert);
+        assocs = (Button)findViewById(R.id.intentAssoc);
+
+        // DATA PASSEE PAR LA REQUETE API
+        try {
+            new RequestHandler(url, "GET", sharedPreferences.getString("token", null), new RequestCallback() {
+            @Override
+                public void run(){
+                   //recupQuant = response.get("");
+                   //recupSpent = response.get("");
+            }
+            });
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+        // Recuperation et Affectation de la valeur des tokens prealablement
+        // recuperes par l'utilisateur
+        this.quant = (TextView) findViewById(R.id.landpage_amount_coin);
+        this.quant.setText(recupQuant);
+
+        // Recuperation et Affectation de la valeur du total depense prealablement
+        // recuperes par l'utilisateur
+        this.totalSpent = (TextView) findViewById(R.id.landpage_amount_spent);
+        this.totalSpent.setText(recupSpent);
+
+        disconnect.setOnClickListener(v ->{
+            Intent backIntent = new Intent(LandpageActivity.this, MainActivity.class);
+            SharedPreferences.Editor ed = sharedPreferences.edit();
+            ed.remove("token");
+            ed.apply();
+            Toast.makeText(LandpageActivity.this, "successfully disconnected", Toast.LENGTH_LONG).show();
+            startActivity(backIntent);
+        });
+
+        convert.setOnClickListener(v ->{
+            //TODO: REQUETE CONVERTION TOKEN - FORMULE
+            //toConvert  = 58€
+            //coins     += Math.floor(toConvert / 10) = 5 coins
+            //toConvert -= toConvert % 10             = 8 €
+
+        });
+
+        assocs.setOnClickListener(v -> {
+            Intent assocIntent = new Intent(LandpageActivity.this, AssocActivity.class);
+            startActivity(assocIntent);
+        });
+    }
 
 }
